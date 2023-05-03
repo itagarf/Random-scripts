48e28a35d41b21d42a6ca549ecab5ad30678043e
itagarf <itagarfavour@gmail.com>
2023-05-03 16:52:29 +0100

    Script to automate git push

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -10,13 +10,13 @@ echo "Enter your message"
 read message
 git add .
 git commit -m"${message}"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "${green}OK!${reset} 👍🏾"
-else
+#if [ -n "$(git status - porcelain)" ];
+#then
+# echo "${green}OK!${reset} 👍🏾"
+#else
  git status
  echo "${green}Pushing to server...${reset}"
- git push -u origin main
+ git push
  echo "{green}Pushed!!!${reset}"
-fi
+#fi
 
