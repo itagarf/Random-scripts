cd9d5bdb2c102a05473f2fdfb6f5b93402ef8229
itagarf <itagarfavour@gmail.com>
2023-05-05 10:00:26 +0100

    Cronjob update

--- cronjob-update.sh
+++ cronjob-update.sh
@@ -2,4 +2,7 @@
 
 echo "This bash script is actually a cronjob running on $(date)"
 
+# The output of this is in a file named cronjob-update.txt. A cronjob directed the output to that file while another cronjob updated github with the change every minute as both jobs ran every minute.
+# The script for this specific github push scheduled to run by a cronjob is not included in this git repository.
+
 
